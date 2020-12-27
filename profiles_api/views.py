66e633d9a4e60bbdc138a1598025e5c47dc71bcc
Satyabrat Panda <satyabratp.m.2000@gmail.com>
2020-12-28 00:01:47 +0530

    Added APIVIew

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from . import serializers
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+    	serializer = self.serializer_class(data = request.data)
+
+    	if(serializer.is_valid()):
+    		name = serializer.validated_data['name']
+    		message = f'Hello {name}'
+    		return Response({'message' : message})
+
+    	return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk = None):
+        """ Handle updating an object """
+        return Response({"message" : "PUT"})
+
+    def patch(self, request, pk = None):
+        """ Handle a partial update of an object """
+        return Response({"message" : "PATCH"})
+
+    def delete(self, request, pk = None):
+        return Response({"message" : "DELETE"})
